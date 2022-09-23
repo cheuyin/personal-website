7934e25498f2eb5ceb86130aa1233c4aa66d1446
cheuyin <yinstanleycheung@gmail.com>
2022-09-23 07:14:37 -0700

    Create project item component and add project pics

--- src/pages/Projects.js
+++ src/pages/Projects.js
@@ -1,7 +1,16 @@
 import React from "react";
+import ProjectItem from "../components/ProjectItem";
 
 function Projects() {
-	return <div>Projects</div>;
+	return (
+		<div className="projects">
+			<h1>My Personal Projects</h1>
+			<div className="projectList">
+				<ProjectItem name="Social Media Website" image={}/>
+				<ProjectItem />
+			</div>
+		</div>
+	);
 }
 
 export default Projects;
