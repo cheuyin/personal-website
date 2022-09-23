ae58104fc3dab32827074830c317af2cf87da83e
cheuyin <yinstanleycheung@gmail.com>
2022-09-22 17:26:54 -0700

    Finalize Experience page

--- src/pages/Experience.js
+++ src/pages/Experience.js
@@ -5,6 +5,7 @@ import {
 } from "react-vertical-timeline-component";
 import "react-vertical-timeline-component/style.min.css";
 import SchoolIcon from "@material-ui/icons/School";
+import "../styles/Experience.css";
 
 function Experience() {
 	return (
