8c3c41827a1066dc0900b7590b1b23835af2fb72
cheuyin <yinstanleycheung@gmail.com>
2022-10-06 19:38:43 -0700

    Update footer links

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,15 +1,26 @@
 import React from "react";
-import { Instagram, Twitter, Facebook, LinkedIn } from "@material-ui/icons/";
+import { LinkedIn, Email, GitHub } from "@material-ui/icons";
 import "../styles/Footer.css";
 
 function Footer() {
 	return (
 		<div className="footer">
 			<div className="socialMedia">
-				<Instagram />
-				<Twitter />
-				<Facebook />
-				<LinkedIn />
+				<address>
+					<a
+						href="https://www.linkedin.com/in/yinstanleycheung/"
+						target="_blank"
+						rel="noreferrer"
+					>
+						<LinkedIn />
+					</a>
+					<a href="mailto: yinstanleycheung@gmail.com">
+						<Email />
+					</a>
+					<a href="https://github.com/cheuyin" target="_blank" rel="noreferrer">
+						<GitHub />
+					</a>
+				</address>
 			</div>
 			<p> &copy; 2022 cheungstanley.com</p>
 		</div>
