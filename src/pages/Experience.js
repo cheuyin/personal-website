e48c81e65d49f7c6066a1726378180572b9b233d
cheuyin <yinstanleycheung@gmail.com>
2022-10-07 19:46:23 -0700

    Add university details and logo  to experience page

--- src/pages/Experience.js
+++ src/pages/Experience.js
@@ -4,7 +4,7 @@ import {
 	VerticalTimelineElement,
 } from "react-vertical-timeline-component";
 import "react-vertical-timeline-component/style.min.css";
-import SchoolIcon from "@material-ui/icons/School";
+import UBCIcon from "../assets/ubc-logo.png";
 import "../styles/Experience.css";
 
 function Experience() {
@@ -13,14 +13,27 @@ function Experience() {
 			<VerticalTimeline lineColor="#3e497a">
 				<VerticalTimelineElement
 					className="vertical-timeline-element--education"
-					date="2010 - 2014"
-					iconStyle={{ background: "#3e497a", color: "#fff" }}
-					icon={<SchoolIcon />}
+					date="2022 - 2026"
+					iconStyle={{
+						display: "flex",
+						alignItems: "center",
+						justifyContent: "center",
+						background: "white",
+						boxShadow:
+							"0 0 0 4px rgb(62, 73, 122), inset 0 2px 0 rgba(62, 73, 122, 0.8), 0 3px 0 4px rgba(62, 73, 122, 0.5)",
+					}}
+					icon={
+						<img
+							src={UBCIcon}
+							alt="UBC logo"
+							style={{ marginTop: "5px", width: "50%", height: "auto" }}
+						></img>
+					}
 				>
 					<h3 className="vertical-timeline-element--title">
-						My Random High School, Random Place, Random State
+						The University of British Columbia, Vancouver, Canada
 					</h3>
-					<p>High School Diploma</p>
+					<p>B.Sc, Honours in Computer Science (intended)</p>
 				</VerticalTimelineElement>
 			</VerticalTimeline>
 		</div>
