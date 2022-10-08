d58c9630baf38398fdd26d990e13aebfa562cc73
cheuyin <yinstanleycheung@gmail.com>
2022-10-07 19:02:42 -0700

    Update projects page with 3 personal projects

--- src/helpers/ProjectList.js
+++ src/helpers/ProjectList.js
@@ -1,4 +1,6 @@
 import Proj1 from "../assets/proj1.png";
+import Proj2 from "../assets/proj2.png";
+import Proj3 from "../assets/proj3.png";
 
 const ProjectList = [
 	{
@@ -7,6 +9,18 @@ const ProjectList = [
 		skills: "JavaScript, HTML, CSS",
 		gitHubLink: "https://github.com/cheuyin/etch-a-sketch"
 	},
+	{
+		name: "Calculator",
+		image: Proj2,
+		skills: "JavaScript, HTML, CSS",
+		gitHubLink: "https://github.com/cheuyin/odin-calculator"
+	},
+	{
+		name: "Simon Game",
+		image: Proj3,
+		skills: "JavaScript, HTML, CSS",
+		gitHubLink: "https://github.com/cheuyin/simon-game"
+	}
 ];
 
 export default ProjectList;
