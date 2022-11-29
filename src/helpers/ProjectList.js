7ebfff842488efb9f7d029918da836068f276753
cheuyin <yinstanleycheung@gmail.com>
2022-11-29 11:28:41 -0800

    Add a project

--- src/helpers/ProjectList.js
+++ src/helpers/ProjectList.js
@@ -1,8 +1,15 @@
 import Proj1 from "../assets/proj1.png";
 import Proj2 from "../assets/proj2.png";
 import Proj3 from "../assets/proj3.png";
+import NodeFilmsPic from "../assets/node-films.png";
 
 const ProjectList = [
+	{
+		name: "Node Films",
+		image: NodeFilmsPic,
+		skills: "Node.js, REST API, Asyncronous programming / Promises",
+		gitHubLink: "https://github.com/cheuyin/node-films"
+	},
 	{
 		name: "Etch-a-Sketch",
 		image: Proj1,
